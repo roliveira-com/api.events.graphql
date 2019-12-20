34fd4681ad69d6b1ecc8965145fc62faff089a97
Rodrigo Oliveira <rodrigooliveira@rodrigo-2.home>
2019-12-20 06:54:49 -0300

    implement resolver and schemas initial setup

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const parser = require('body-parser');
+const graphqlHandler = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 const app = express();
 
 app.use(parser.json());
+app.use('/query', graphqlHandler({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['a', 'b', 'c']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    }
+  },
+  graphiql: true
+}))
 
 app.get('/', (req, res, next) => {
   res.send('Thats Works');
